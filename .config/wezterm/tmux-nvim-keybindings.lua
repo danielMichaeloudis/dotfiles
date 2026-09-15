6c919e69c5682f7dfa05d82002b5986788948b9c
Daniel Michaeloudis <dmichaeloudis@gmail.com>
2026-09-16 00:03:42 +0100

    remove powershell specific code

--- .config/wezterm/tmux-nvim-keybindings.lua
+++ .config/wezterm/tmux-nvim-keybindings.lua
@@ -1,31 +1,30 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 local M = {}
 
 M.keys = {
-  {
-    key = "h",
-    mods = "ALT|SHIFT",
-    action= act.ActivateTabRelative(-1),
-  },
-  {
-    key = "l",
-    mods = "ALT|SHIFT",
-    action= act.ActivateTabRelative(1)
-  },
-  {
-    key = "c",
-    mods = "LEADER",
-    action = act.SpawnTab 'CurrentPaneDomain',
-  },
-  {
-    key = "x",
-    mods = "LEADER",
-    action = act.CloseCurrentTab { confirm = true },
-  }
-
+	{
+		key = "h",
+		mods = "ALT|SHIFT",
+		action = act.ActivateTabRelative(-1),
+	},
+	{
+		key = "l",
+		mods = "ALT|SHIFT",
+		action = act.ActivateTabRelative(1),
+	},
+	{
+		key = "c",
+		mods = "LEADER",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "x",
+		mods = "LEADER",
+		action = act.CloseCurrentTab({ confirm = true }),
+	},
 }
 
 return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.default_prog = { 'pwsh.exe' }
+-- config.default_prog = { 'pwsh.exe' }
 
 -- Initial window setup.
 config.initial_cols = 120
@@ -11,13 +11,12 @@ config.initial_rows = 28
 
 -- Colours and font.
 config.font_size = 12
-config.color_scheme = 'nord'
-config.font = wezterm.font 'FantasqueSansM Nerd Font'
+config.color_scheme = "nord"
+config.font = wezterm.font("FantasqueSansM Nerd Font")
 
 -- Keybindings.
-config.leader = {key = "Space", mods = "CTRL"}
+config.leader = { key = "Space", mods = "CTRL" }
 local tmux_keys = require("tmux-nvim-keybindings")
 config.keys = tmux_keys.keys
-
 
 return config
